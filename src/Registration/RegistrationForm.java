2960284a8950f08802ccbb1ff68f73c64cc10a85
Adlan Leah Kirumira <adlanleah01@gmail.com>
2025-06-12 00:30:41 +0300

    Update RegistrationForm.java

--- src/Registration/RegistrationForm.java
+++ src/Registration/RegistrationForm.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author ADLAN
- */
- package Registration;
+package Registration;
 
 import java.awt.Image;
 import java.io.File;
